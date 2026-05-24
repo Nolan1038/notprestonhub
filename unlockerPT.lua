673fb024d53af97904401a29349570abbfcca9dd
Nolan1038 <90103911+Nolan1038@users.noreply.github.com>
2026-05-23 21:32:37 -0500

    Implement game unlocker for Pop It Trading
    Add unlocker script for specific game place ID.

---
+++ unlockerPT.lua
@@ -0,0 +1,11 @@
+local allowedPlaceId = 7346416636
+
+if game.PlaceId ~= allowedPlaceId then
+    game.Players.LocalPlayer:Kick("This game isn't supported with this script!")
+    return
+end
+
+print("Pop It Trading Unlocker ehehhehehehe")
+game.Players.LocalPlayer.XRay.Value = true
+game.Workspace.Map.VIP:Destroy()
+game.Workspace.GoldenToilet.HitDetect:Destroy()
